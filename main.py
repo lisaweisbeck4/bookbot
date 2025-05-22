cce7c7b82f95b56b5cc197b6968bccbe74d2ff3a
geauxweisbeck4 <andrew.weisbeck@gmail.com>
2025-05-22 18:16:55 -0400

    Completed Bookbot project for boot.dev

---
+++ main.py
@@ -0,0 +1,27 @@
+from stats import count_words, count_characters, sort_on, sort_dictionary
+import os.path, sys
+
+def main():
+  if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    exit(1)
+  filepath = sys.argv[1]
+
+  try:
+      word_count = count_words(filepath)
+  except Exception as e:
+      print(e)
+      main()
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {filepath}")
+  print("----------- Word Count ----------")
+  print(f"Found {word_count} total words")
+  print("--------- Character Count -------")
+  entries = count_characters(filepath)
+  entries = sort_dictionary(entries)
+  for entry in entries:
+    if entry["char"].isalpha():
+      print(f"{entry["char"]}: {entry["num"]}")
+  print("============= END ===============")
+
+main()
---
+++ stats.py
@@ -0,0 +1,36 @@
+import os.path
+
+def get_book_text(filepath):
+  if os.path.isfile(filepath) == False:
+    raise Exception("File path is invalid.")
+  with open(filepath) as f:
+    f = f.read()
+    return f
+def count_words(filepath):
+    counter = 0
+    text = get_book_text(filepath)
+    text = text.split()
+    for word in text:
+        counter += 1
+    return counter
+
+def count_characters(filepath):
+    text = get_book_text(filepath).lower()
+    letter_count = {}
+    for letter in text:
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+    return letter_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dictionary(dictionary):
+    dict_list = []
+    for k in dictionary:
+        k_dict = {"char":k,"num":dictionary[k]}
+        dict_list.append(k_dict)
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
